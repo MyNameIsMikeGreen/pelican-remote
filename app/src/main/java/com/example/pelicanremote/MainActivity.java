1af6a5dd74053c1a7dfde381db122849ed6fb1a4
mikegreen1995 <mikegreen1995@outlook.com>
2020-04-09 21:35:48 +0100

    Add refresh status button. Pretty print responses.

--- app/src/main/java/com/example/pelicanremote/MainActivity.java
+++ app/src/main/java/com/example/pelicanremote/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,32 +19,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.activateButton).setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v) {
-                new PelicanRequest().execute(getString(R.string.endpoint_activate));
-            }
-        });
+        setStatusRefreshButtonClickListener();
+        setStatusToggleButtonClickListener(R.id.activateButton, getString(R.string.endpoint_activate));
+        setStatusToggleButtonClickListener(R.id.deactivateButton, getString(R.string.endpoint_deactivate));
 
-        findViewById(R.id.deactivateButton).setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v) {
-                new PelicanRequest().execute(getString(R.string.endpoint_deactivate));
-            }
-        });
+    }
 
-        findViewById(R.id.statusButton).setOnClickListener(new View.OnClickListener()
+    private void setStatusRefreshButtonClickListener() {
+        findViewById(R.id.refreshStatusButton).setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v) {
                 AsyncTask<String, String, String> result = new PelicanRequest().execute(getString(R.string.endpoint_status));
                 try {
-                    String responseBody = result.get();
-                    TextView tv = findViewById(R.id.resultText);
-                    tv.setText(responseBody);
-                } catch (ExecutionException | InterruptedException e) {
+                    String serverResponse = result.get();
+                    printResponse(serverResponse);
+                    JSONObject statusJson = new JSONObject(serverResponse);
+                    String status = statusJson.getString("status");
+                    TextView statusResultLabel = findViewById(R.id.status_result_label);
+                    statusResultLabel.setText(status);
+                } catch (ExecutionException | InterruptedException | JSONException e) {
                     e.printStackTrace();
                 }
             }
         });
     }
+
+    private void setStatusToggleButtonClickListener(final int buttonId, final String endpoint) {
+        findViewById(buttonId).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AsyncTask<String, String, String> result = new PelicanRequest().execute(endpoint);
+                String serverResponse = null;
+                try {
+                    serverResponse = result.get();
+                    printResponse(serverResponse);
+                } catch (ExecutionException | InterruptedException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void printResponse(String response) throws JSONException {
+        JSONObject statusJson = new JSONObject(response);
+        TextView resultBox = findViewById(R.id.resultText);
+        resultBox.setText(statusJson.toString(4));
+    }
+
 }
