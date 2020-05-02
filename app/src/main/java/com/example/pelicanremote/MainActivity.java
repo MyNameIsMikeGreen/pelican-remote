66b748d7184ef14269a4165c7316137759f6aaf3
mikegreen1995 <mikegreen1995@outlook.com>
2020-05-02 14:49:34 +0100

    Parse more response items. Altered layout.

--- app/src/main/java/com/example/pelicanremote/MainActivity.java
+++ app/src/main/java/com/example/pelicanremote/MainActivity.java
@@ -6,13 +6,14 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ExecutionException;
 
 import static com.example.pelicanremote.PelicanUrlBuilder.Endpoint;
@@ -29,9 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        makeResponseBoxScrollable();
-
         setStatusToggleButtonClickListener(R.id.activateButton, urlBuilder.build(Endpoint.ACTIVATE));
         setStatusToggleButtonClickListener(R.id.deactivateButton, urlBuilder.build(Endpoint.DEACTIVE));
 
@@ -60,52 +58,86 @@ public class MainActivity extends AppCompatActivity {
 
     private void refreshStatus() {
         AsyncTask<String, String, String> result = new PelicanRequest().execute(urlBuilder.build(Endpoint.STATUS));
-        TextView statusResultLabel = findViewById(R.id.status_result_label);
+
         try {
             String serverResponse = result.get();
-            String status = new JSONObject(serverResponse).getString("status");
-            statusResultLabel.setText(status);
-            findViewById(R.id.activateButton).setEnabled(true);
-            findViewById(R.id.deactivateButton).setEnabled(true);
-            if (status.equals("ACTIVATED")){
-                statusResultLabel.setTextColor(Color.GREEN);
-            }
-            else {
-                statusResultLabel.setTextColor(Color.RED);
-            }
+            populateStatusLabels(serverResponse);
         } catch (ExecutionException | InterruptedException | JSONException e) {
             e.printStackTrace();
-            statusResultLabel.setText(getString(R.string.status_not_connected));
-            statusResultLabel.setTextColor(Color.GRAY);
+            changeElementsToNotConnectedMode();
+        }
+    }
+
+    private void changeElementsToNotConnectedMode(){
+        TextView statusResultLabel = findViewById(R.id.status_result_label);
+        statusResultLabel.setText(getString(R.string.status_not_connected));
+        statusResultLabel.setTextColor(Color.GRAY);
+        findViewById(R.id.activateButton).setEnabled(false);
+        findViewById(R.id.deactivateButton).setEnabled(false);
+        TextView lastChangeResultLabel = findViewById(R.id.last_change_result_label);
+        lastChangeResultLabel.setText(R.string.last_change_not_found);
+        TextView lastChangeByResultLabel = findViewById(R.id.last_change_by_result_label);
+        lastChangeByResultLabel.setText(R.string.last_change_not_found);
+    }
+
+    private void populateStatusLabels(String serverResponse) throws JSONException {
+        JSONObject serverResponseJson = new JSONObject(serverResponse);
+        setStatusLabelText(serverResponseJson);
+        setLastChangeLabelText(serverResponseJson);
+        setLastChangeByLabelText(serverResponseJson);
+        disableAppropriateActionButton(serverResponseJson);
+    }
+
+    private void disableAppropriateActionButton(JSONObject serverResponseJson) throws JSONException {
+        String status = serverResponseJson.getString("status");
+        if (status.equals("ACTIVATED")){
             findViewById(R.id.activateButton).setEnabled(false);
+            findViewById(R.id.deactivateButton).setEnabled(true);
+        }
+        if (status.equals("DEACTIVATED")){
+            findViewById(R.id.activateButton).setEnabled(true);
             findViewById(R.id.deactivateButton).setEnabled(false);
         }
+        else {
+            // Enable both buttons so that actions can be performed as a backup
+            findViewById(R.id.activateButton).setEnabled(true);
+            findViewById(R.id.deactivateButton).setEnabled(true);
+        }
+    }
+
+    private void setStatusLabelText(JSONObject serverResponseJson) throws JSONException {
+        String status = serverResponseJson.getString("status");
+        TextView statusResultLabel = findViewById(R.id.status_result_label);
+        statusResultLabel.setText(status);
+        if (status.equals("ACTIVATED")){
+            statusResultLabel.setTextColor(Color.GREEN);
+        }
+        else {
+            statusResultLabel.setTextColor(Color.RED);
+        }
+    }
+
+    private void setLastChangeLabelText(JSONObject serverResponseJson) throws JSONException {
+        String lastChange = serverResponseJson.getString("lastChange");
+        DateTimeFormatter incomingFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.n");
+        LocalDateTime dateTime = LocalDateTime.parse(lastChange, incomingFormat);
+        DateTimeFormatter outgoingFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd @ HH:mm:ss");
+        TextView lastChangeResultLabel = findViewById(R.id.last_change_result_label);
+        lastChangeResultLabel.setText(dateTime.format(outgoingFormat));
+    }
+
+    private void setLastChangeByLabelText(JSONObject serverResponseJson) throws JSONException {
+        String lastChangeBy = serverResponseJson.getString("lastChangeBy");
+        TextView lastChangeByResultLabel = findViewById(R.id.last_change_by_result_label);
+        lastChangeByResultLabel.setText(lastChangeBy);
     }
 
     private void setStatusToggleButtonClickListener(final int buttonId, final String endpoint) {
         findViewById(buttonId).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                AsyncTask<String, String, String> result = new PelicanRequest().execute(endpoint);
-                String serverResponse = null;
-                try {
-                    serverResponse = result.get();
-                    printResponse(serverResponse);
-                } catch (ExecutionException | InterruptedException | JSONException e) {
-                    e.printStackTrace();
-                }
+                new PelicanRequest().execute(endpoint);
             }
         });
-    }
-
-    private void printResponse(String response) throws JSONException {
-        JSONObject statusJson = new JSONObject(response);
-        TextView resultBox = findViewById(R.id.responseBox);
-        resultBox.setText(statusJson.toString(4));
-    }
-
-    private void makeResponseBoxScrollable() {
-        TextView resultText = findViewById(R.id.responseBox);
-        resultText.setMovementMethod(new ScrollingMovementMethod());
     }
 
 }
