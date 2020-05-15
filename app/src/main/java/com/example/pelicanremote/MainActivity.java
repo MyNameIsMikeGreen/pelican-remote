7a894f3d1df4c15b575dfa5876cabebbdbb4fb86
mikegreen1995 <mikegreen1995@outlook.com>
2020-05-15 17:15:05 +0100

    Support for MODIFYING state. Sample settings page from toolbar.

--- app/src/main/java/com/example/pelicanremote/MainActivity.java
+++ app/src/main/java/com/example/pelicanremote/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.pelicanremote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,6 +25,12 @@ import static com.example.pelicanremote.PelicanUrlBuilder.Endpoint;
 public class MainActivity extends AppCompatActivity {
 
     private final static int STATUS_POLL_INTERVAL_MILLIS = 3000;
+    public static final String ACTIVATED = "ACTIVATED";
+    public static final String DEACTIVATED = "DEACTIVATED";
+    public static final String MODIFYING = "MODIFYING";
+    public static final String STATUS = "status";
+    public static final String LAST_CHANGE = "lastChange";
+    public static final String LAST_CHANGE_BY = "lastChangeBy";
 
     private final PelicanUrlBuilder urlBuilder = new PelicanUrlBuilder("http", "192.168.5.80", 8000);
     private final Handler statusHandler = new Handler();
@@ -30,9 +40,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
         setStatusToggleButtonClickListener(R.id.activateButton, urlBuilder.build(Endpoint.ACTIVATE));
         setStatusToggleButtonClickListener(R.id.deactivateButton, urlBuilder.build(Endpoint.DEACTIVE));
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+            }
+        }
+        return true;
     }
 
     @Override
@@ -89,36 +117,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void disableAppropriateActionButton(JSONObject serverResponseJson) throws JSONException {
-        String status = serverResponseJson.getString("status");
-        if (status.equals("ACTIVATED")){
-            findViewById(R.id.activateButton).setEnabled(false);
-            findViewById(R.id.deactivateButton).setEnabled(true);
-        }
-        else if (status.equals("DEACTIVATED")){
-            findViewById(R.id.activateButton).setEnabled(true);
-            findViewById(R.id.deactivateButton).setEnabled(false);
-        }
-        else {
-            // Enable both buttons so that actions can be performed as a backup
-            findViewById(R.id.activateButton).setEnabled(true);
-            findViewById(R.id.deactivateButton).setEnabled(true);
+        String status = serverResponseJson.getString(STATUS);
+        switch (status) {
+            case ACTIVATED:
+                findViewById(R.id.activateButton).setEnabled(false);
+                findViewById(R.id.deactivateButton).setEnabled(true);
+                break;
+            case DEACTIVATED:
+                findViewById(R.id.activateButton).setEnabled(true);
+                findViewById(R.id.deactivateButton).setEnabled(false);
+                break;
+            case MODIFYING:
+                findViewById(R.id.activateButton).setEnabled(false);
+                findViewById(R.id.deactivateButton).setEnabled(false);
+                break;
+            default:
+                // Enable both buttons so that actions can be performed as a backup
+                findViewById(R.id.activateButton).setEnabled(true);
+                findViewById(R.id.deactivateButton).setEnabled(true);
+                break;
         }
     }
 
     private void setStatusLabelText(JSONObject serverResponseJson) throws JSONException {
-        String status = serverResponseJson.getString("status");
+        String status = serverResponseJson.getString(STATUS);
         TextView statusResultLabel = findViewById(R.id.status_result_label);
         statusResultLabel.setText(status);
-        if (status.equals("ACTIVATED")){
-            statusResultLabel.setTextColor(Color.GREEN);
-        }
-        else {
-            statusResultLabel.setTextColor(Color.RED);
+        switch (status) {
+            case ACTIVATED:
+                statusResultLabel.setTextColor(Color.GREEN);
+                break;
+            case DEACTIVATED:
+                statusResultLabel.setTextColor(Color.RED);
+                break;
+            case MODIFYING:
+                statusResultLabel.setTextColor(Color.BLUE);
+                break;
+            default:
+                statusResultLabel.setTextColor(Color.GRAY);
+                break;
         }
     }
 
     private void setLastChangeLabelText(JSONObject serverResponseJson) throws JSONException {
-        String lastChange = serverResponseJson.getString("lastChange");
+        String lastChange = serverResponseJson.getString(LAST_CHANGE);
         DateTimeFormatter incomingFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.n");
         LocalDateTime dateTime = LocalDateTime.parse(lastChange, incomingFormat);
         DateTimeFormatter outgoingFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd\nHH:mm:ss");
@@ -127,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setLastChangeByLabelText(JSONObject serverResponseJson) throws JSONException {
-        String lastChangeBy = serverResponseJson.getString("lastChangeBy");
+        String lastChangeBy = serverResponseJson.getString(LAST_CHANGE_BY);
         TextView lastChangeByResultLabel = findViewById(R.id.last_change_by_result_label);
         lastChangeByResultLabel.setText(lastChangeBy);
     }
