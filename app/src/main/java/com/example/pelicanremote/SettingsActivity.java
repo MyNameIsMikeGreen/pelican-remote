fc5af820d177ae12c54bcd14b9faaf2278b15f30
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-03-10 20:46:35 +0000

    Remove splash screen from settings menu background

--- app/src/main/java/com/example/pelicanremote/SettingsActivity.java
+++ app/src/main/java/com/example/pelicanremote/SettingsActivity.java
@@ -10,6 +10,7 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
         getSupportFragmentManager()
