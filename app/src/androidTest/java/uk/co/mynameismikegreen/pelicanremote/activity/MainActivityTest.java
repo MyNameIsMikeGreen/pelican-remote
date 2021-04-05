21bec90e26a4863e9b24ac2cceb7b12229ece9b3
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-04-05 10:37:40 +0100

    Conditionally wait for activities to fully load. Add activated server test.

--- app/src/androidTest/java/uk/co/mynameismikegreen/pelicanremote/activity/MainActivityTest.java
+++ app/src/androidTest/java/uk/co/mynameismikegreen/pelicanremote/activity/MainActivityTest.java
@@ -9,6 +9,8 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import junit.framework.AssertionFailedError;
+
 import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,16 +20,20 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalTime;
 import java.util.stream.Collectors;
 
-import uk.co.mynameismikegreen.pelicanremote.R;
 import uk.co.mynameismikegreen.pelicanremote.MockPelicanServerTestSuper;
+import uk.co.mynameismikegreen.pelicanremote.R;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static java.lang.Thread.sleep;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
@@ -40,14 +46,16 @@ public class MainActivityTest extends MockPelicanServerTestSuper {
 
     @Test
     public void notConnectedToServer() throws InterruptedException {
-        // Given: App is configured to use a non-existent server
+        // Given: App is launched
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         Editor preferencesEditor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+
+        // And: App is configured to use a non-existent server
         preferencesEditor.putString("address", BAD_ADDRESS);
         preferencesEditor.commit();
 
-        // When: The activity is launched
-        Thread.sleep(1000);
+        // When: The app displays the correct label
+        waitForLabel(R.id.status_result_label, "NOT CONNECTED");
 
         // Then: All buttons are visible but not enabled
         assertButtonState(R.id.activateButton, true, false);
@@ -68,15 +76,36 @@ public class MainActivityTest extends MockPelicanServerTestSuper {
         preferencesEditor.putString("port", String.valueOf(getMockPelicanServerPort()));
         preferencesEditor.commit();
 
-        // When: The activity is launched
-        Thread.sleep(3000);
+        // When: The app displays the correct label
+        waitForLabel(R.id.status_result_label, "DEACTIVATED");
 
         // Then: Only activation buttons are enabled
         assertButtonState(R.id.activateButton, true, true);
         assertButtonState(R.id.activateUntilMidnightButton, true, true);
         assertButtonState(R.id.deactivateButton, true, false);
         assertButtonState(R.id.rescanButton, true, false);
+    }
 
+    @Test
+    public void activatedServer() throws InterruptedException {
+        // Given: App is launched
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Editor preferencesEditor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+
+        // And: App is configured to use an activated server
+        setPelicanStub("/status", readResource("status_response_activated.json"));
+        preferencesEditor.putString("address", getMockPelicanServerHost());
+        preferencesEditor.putString("port", String.valueOf(getMockPelicanServerPort()));
+        preferencesEditor.commit();
+
+        // When: The app displays the correct label
+        waitForLabel(R.id.status_result_label, "ACTIVATED");
+
+        // Then: Only the activate buttons are disabled
+        assertButtonState(R.id.activateButton, true, false);
+        assertButtonState(R.id.activateUntilMidnightButton, true, false);
+        assertButtonState(R.id.deactivateButton, true, true);
+        assertButtonState(R.id.rescanButton, true, true);
     }
 
     private void assertButtonState(int buttonId, boolean visible, boolean enabled) {
@@ -93,6 +122,25 @@ public class MainActivityTest extends MockPelicanServerTestSuper {
                 .lines()
                 .collect(Collectors.joining("\n"));
 
+    }
+
+    private void waitForLabel(int labelId, String labelValue) throws InterruptedException {
+        waitForLabel(labelId, labelValue, 5, 1);
+    }
+
+    private void waitForLabel(int labelId, String labelValue, int timeoutSeconds, int pollIntervalSeconds) throws InterruptedException {
+        LocalTime startTime = LocalTime.now();
+        while(true) {
+            try {
+                onView(withId(labelId)).check(matches(withText(is(labelValue))));
+                return;
+            } catch (AssertionFailedError ex) {
+                if (LocalTime.now().isAfter(startTime.plusSeconds(timeoutSeconds))) {
+                    throw ex;
+                }
+            }
+            sleep(pollIntervalSeconds * 1000);
+        }
     }
 
 }
